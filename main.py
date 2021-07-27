46c0a95ad8e233f9d730fdbb34c921a03195eab0
Basavaraj2k00 <basavaraj2k00@gmail.com>
2021-07-27 20:37:11 +0530

    changed wrapping text in cards

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ import os
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# app.config['SECRET_KEY'] = "mysecretkeybro"
+
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///todo.db")
